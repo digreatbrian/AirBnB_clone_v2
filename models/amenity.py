9087a6ca00c33a4a0dc0d10fa40c9c17ca48dfbf
digreatbrian <digreatbrian@gmail.com>
2024-03-20 17:48:44 -0700

    Reverting last changesm

--- models/amenity.py
+++ models/amenity.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-"""
-    module containing Amenity class
-"""
-from models.base_model import BaseModel, Base
-from models.city import City
-from models.user import User
-from models.place import place_amenity
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from airbnb.settings import MODELS_DIR
+from airbnb.settings import DB_MODELS_DIR
+from airbnb._import import _import
 
 
-class Amenity(BaseModel, Base):
-    """
-        Amenity class
-    """
+class Amenity(BaseModel):
+    name = ""
 
-    if (storage_engine == "db"):
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place",
-            secondary=place_amenity, back_populates="amenities")
-    else:
-        name = ""
+if MODELS_DIR == DB_MODELS_DIR:
+    Amenity = _import(MODELS_DIR + f".amenity.Amenity")
+
--- models/state.py
+++ models/state.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-"""
-    contains state class to represent a state
-"""
-
-from models.base_model import BaseModel, Base
-import models
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from airbnb.settings import MODELS_DIR
+from airbnb.settings import DB_MODELS_DIR
+from airbnb._import import _import
 
 
-class State(BaseModel, Base):
-    """ State class: class to represent states of cities"""
-    if (storage_engine == 'db'):
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+class State(BaseModel):
+    """ State class """
+    name = ""
 
-        @property
-        def cities(self):
-            """cities list
-            """
-            result = []
-            for j, i in models.storage.all(models.city.City).items():
-                if (i.state_id == self.id):
-                    result.append(i)
-            return result
+if MODELS_DIR == DB_MODELS_DIR:
+    State = _import(MODELS_DIR + f".state.State")
+
